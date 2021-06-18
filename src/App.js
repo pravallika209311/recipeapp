10de060c4d59de3f9af973e96c395bbe6e9511e6
pravallika <aketypravallika@users.noreply.github.com>
2021-06-18 17:27:08 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import {useEffect ,useState} from 'react';
 function App() {
-  return (
+   const [ingredientList,updateIngredientList]= useState([]);
+  const API_KEY ="8569e1b422938159708c866456d4520d";
+const APP_ID = "644a0e1c"; 
+
+useEffect(() => {
+  let url = `search?q=desserts&app_id=${APP_ID}&app_key=${API_KEY}`;
+  fetch(url)
+  .then(response=>{
+  console.log(response);
+  return response.json();
+  })
+  .then(res=>{
+    console.log(res.hits,res)
+   updateIngredientList(res.hits);
+  })
+  .catch(err=> console.log("error",err) );  
+},[]);
+
+  return ( 
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="wrapper"></div>
+       {ingredientList.map((item)=>{
+         return (
+           <div className="Ingredient">
+            <span>{item.recipe.label}</span>
+            <img src={item.recipe.image}/>
+            {item.recipe.ingredientLines.map(step=>{
+           return  <p>{step}</p>
+            })}
+           </div>
+         )
+       })}
       </header>
     </div>
-  );
+  ); 
 }
 
 export default App;
+ 
