cf89e0c2b4697ad25ce078d67c174f61b6e4043f
pravallika <aketypravallika@users.noreply.github.com>
2021-06-24 23:17:43 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
-import {useEffect ,useState} from 'react';
+import {useEffect ,useState,useRef} from 'react';
 function App() {
    const [ingredientList,updateIngredientList]= useState([]);
+   const inputRef = useRef(null);
   const API_KEY ="8569e1b422938159708c866456d4520d";
-const APP_ID = "644a0e1c"; 
-
-useEffect(() => {
-  let url = `search?q=desserts&app_id=${APP_ID}&app_key=${API_KEY}`;
+const APP_ID = "644a0e1c";
+const search=()=>{
+  console.log("input ref" ,inputRef.current.value)
+   searchForRecipie(inputRef.current.value);
+};
+const searchForRecipie=(query)=>{
+  let url = `search?q=${query}&app_id=${APP_ID}&app_key=${API_KEY}`;
   fetch(url)
   .then(response=>{
   console.log(response);
@@ -16,24 +20,38 @@ useEffect(() => {
     console.log(res.hits,res)
    updateIngredientList(res.hits);
   })
-  .catch(err=> console.log("error",err) );  
+  .catch(err=> console.log("error",err) ); 
+
+}
+useEffect(() => {
+searchForRecipie('chicken');
 },[]);
 
   return ( 
     <div className="App">
-      <header className="App-header">
-        <div className="wrapper"></div>
-       {ingredientList.map((item)=>{
+      <header className="App-header" >
+        <div>
+          <input ref={inputRef} placeholder="Search for recipe"/>
+          <button onClick={search}>Search</button>
+          </div>
+        <div className="wrapper">
+       {ingredientList.map((item,index)=>{
          return (
-           <div className="Ingredient">
+           <div className="Ingredient" key={index}>
             <span>{item.recipe.label}</span>
-            <img src={item.recipe.image}/>
-            {item.recipe.ingredientLines.map(step=>{
-           return  <p>{step}</p>
+
+            <img src={item.recipe.image} alt=".."/>
+            <div className="Steps">
+            {item.recipe.ingredientLines.map((step,i)=>{
+           return  <p key={i}>{step}</p>
             })}
+            </div>
            </div>
          )
-       })}
+         
+       }
+       )}
+       </div>
       </header>
     </div>
   ); 
--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import App from './App';
 
 ReactDOM.render(
  
-    <App />
- ,
+    <App /> ,
   document.getElementById('root')
 );
 
